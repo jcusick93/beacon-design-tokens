9b7c49263e197b8a392c634ce8b2fbf8dbd5e48a
Josh Cusick <josh.m.cusick@gmail.com>
2024-09-09 17:24:20 -0400

    Updates

---
+++ fetchTokens.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+const figmaToken = 'figd_kCBwTAnpLscuIZPTi66s14wKWAalvqaYH2IJSP3_';
+const fileKey = 'exVarXASEUOODOdve81okt';
+
+axios.get(`https://api.figma.com/v1/files/${fileKey}/variables/local`, {
+    headers: {
+        'X-Figma-Token': figmaToken
+    }
+})
+.then(response => {
+    const data = response.data;
+    
+    // Extract variable collections
+    const variableCollections = data.meta.variableCollections;
+    console.log('Variable Collections:');
+    for (const [id, collection] of Object.entries(variableCollections)) {
+        console.log(`Collection ID: ${id}`);
+        console.log(`Name: ${collection.name}`);
+        console.log(`Description: ${collection.description}`);
+    }
+
+    // Extract variables
+    const variables = data.meta.variables;
+    console.log('\nVariables:');
+    for (const [id, variable] of Object.entries(variables)) {
+        console.log(`Variable ID: ${id}`);
+        console.log(`Name: ${variable.name}`);
+        console.log(`Type: ${variable.type}`);
+        console.log(`Value: ${JSON.stringify(variable.value)}`);
+    }
+})
+.catch(error => {
+    console.error('Error fetching data:', error);
+});
